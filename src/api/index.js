6dd961eb175549c151d85736f9b71eed52c7a015
hyunin2021 <eehaein2@gmail.com>
2021-02-21 16:48:51 +0900

    최종 배포

--- src/api/index.js
+++ src/api/index.js
@@ -7,5 +7,9 @@ const api = new Router();
 api.use('/posts', posts.routes());
 api.use('/auth', auth.routes());
 
+api.get('/', (ctx) => {
+  ctx.body = 'api 주소 실행중';
+});
+
 // 라우터를 내보냅니다.
 export default api;
--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
-import api from './api';
 import Koa from 'koa';
-import jwtMiddleware from './lib/jwtMiddleware';
-// import createFakeData from './createFakeData';
 
-const app = new Koa();
-const router = new Router();
+// import createFakeData from './createFakeData';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
+
+import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
+
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
@@ -25,12 +28,27 @@ mongoose
     console.error(e);
   });
 
+const app = new Koa();
+const router = new Router();
+
 router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
+
+// router.get('/', (ctx) => {
+//   ctx.body = '홈';
+// });
 
 app.use(bodyParser());
 app.use(jwtMiddleware);
 
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../stovice_react/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
