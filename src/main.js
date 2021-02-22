c6a52d512770fe09dc888402701125b4756a41b5
hyunin2021 <eehaein2@gmail.com>
2021-02-22 23:09:19 +0900

    cors

--- src/main.js
+++ src/main.js
@@ -46,6 +46,15 @@ router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우�
 // });
 
 app.use(cors());
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  await next();
+});
 app.use(bodyParser());
 app.use(jwtMiddleware);
 
